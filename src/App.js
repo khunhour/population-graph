96b8dc2a14031d0cf52705f277b25f7700cf2595
Khunhour Morm <khonhourmorm@gmail.com>
2022-04-07 16:27:36 +0900

    added checked  handling for checked state

--- src/App.js
+++ src/App.js
@@ -2,9 +2,22 @@ import React from "react";
 import CheckList from "./component/CheckList";
 
 export default function App() {
+  const [checked, setChecked] = useState([]);
+
+  // Add/Remove checked item from list
+  const handleCheck = (event) => {
+    var updatedList = [...checked];
+    if (event.target.checked) {
+      updatedList = [...checked, event.target.value];
+    } else {
+      updatedList.splice(checked.indexOf(event.target.value), 1);
+    }
+    setChecked(updatedList);
+  };
+
   return (
     <>
-      <CheckList />
+      <CheckList handleCheck={handleCheck} />
     </>
   );
 }
