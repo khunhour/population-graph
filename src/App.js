024558479dc7e15aef32a66e0454cfa9a29266d6
Khunhour Morm <khonhourmorm@gmail.com>
2022-04-07 16:21:28 +0900

    created a checklist component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from "react";
+import CheckList from "./component/CheckList";
 
 export default function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <CheckList />
+    </>
+  );
 }
