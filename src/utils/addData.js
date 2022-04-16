b4660078824ab77930a5f82150fa87f8d91ed23d
Khunhour Morm <khonhourmorm@gmail.com>
2022-04-16 22:09:22 +0900

    create function to fetch population data

---
+++ src/utils/addData.js
@@ -0,0 +1,13 @@
+export const addData = async (id) => {
+	const data = await fetch(
+		`https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?cityCode=11362&prefCode=${id}`,
+		{
+			headers: {
+				"Content-Type": "application/json;charset=UTF-8",
+				"X-API-KEY": `${process.env.REACT_APP_API_KEY}`,
+			},
+		}
+	);
+	const population = await data.json();
+	return population.result.data[0];
+};
