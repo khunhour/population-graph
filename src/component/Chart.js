fb6946ca1c44163cd0eb9225051e01681fbc166f
Khunhour Morm <khonhourmorm@gmail.com>
2022-04-17 17:02:00 +0900

    create the chart component using recharts

---
+++ src/component/Chart.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { Line, LineChart, ResponsiveContainer, XAxis, YAxis } from "recharts";
+import { colors } from "../utils/colors";
+
+function Chart({ data }) {
+	let lines = [];
+	if (data.length !== 0) {
+		lines = Object.keys(data[0]);
+		lines.shift();
+	}
+
+	return (
+		<ResponsiveContainer width="99%" aspect={3}>
+			<LineChart
+				width={600}
+				height={500}
+				data={data}
+				margin={{
+					top: 10,
+					right: 30,
+					left: 20,
+					bottom: 10,
+				}}
+			>
+				<XAxis dataKey="year" />
+				<YAxis />
+				{lines.map((city, index) => {
+					return (
+						<Line
+							type="monotone"
+							dataKey={city}
+							stroke={colors[index]}
+						/>
+					);
+				})}
+			</LineChart>
+		</ResponsiveContainer>
+	);
+}
+
+export default Chart;
